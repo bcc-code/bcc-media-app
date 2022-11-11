9943e7ca97cad92a9c0eb0f2e6adc546f209ac54
Andreas Gangsø <andreas.gangso@bcc.no>
2022-11-11 15:36:19 +0100

    fix: bottom sheet had transparent bg

--- lib/components/bottom_sheet_select.dart
+++ lib/components/bottom_sheet_select.dart
@@ -38,72 +38,76 @@ class _BottomSheetSelectState extends State<BottomSheetSelect> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: Container(
-                height: 5,
-                width: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: BtvColors.label4,
+    return Container(
+      color: BtvColors.background1,
+      child: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Container(
+                  height: 5,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: BtvColors.label4,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: Text(
-                widget.title,
-                textAlign: TextAlign.center,
-                style: BtvTextStyles.title3,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: OptionList(
-                optionData: widget.items,
-                currentSelection: localSelectedId,
-                onSelectionChange: (val) {
-                  setState(() {
-                    localSelectedId = val;
-                  });
-                  widget.onSelectionChanged(val);
-                  Navigator.pop(
-                      context, BottomSheetSelectResult(cancelled: false));
-                },
-                margin: const EdgeInsets.all(0),
-                enableDivider: false,
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 52.1,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: BtvColors.seperatorOnLight,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(100))),
-                  side: const BorderSide(
-                      width: 1, color: BtvColors.seperatorOnLight),
-                ),
-                onPressed: () {
-                  Navigator.pop(
-                      context, BottomSheetSelectResult(cancelled: true));
-                },
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
                 child: Text(
-                  'Cancel',
-                  style:
-                      BtvTextStyles.button1.copyWith(color: BtvColors.label1),
+                  widget.title,
+                  textAlign: TextAlign.center,
+                  style: BtvTextStyles.title3,
                 ),
               ),
-            ),
-          ],
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: OptionList(
+                  optionData: widget.items,
+                  currentSelection: localSelectedId,
+                  onSelectionChange: (val) {
+                    setState(() {
+                      localSelectedId = val;
+                    });
+                    widget.onSelectionChanged(val);
+                    Navigator.pop(
+                        context, BottomSheetSelectResult(cancelled: false));
+                  },
+                  margin: const EdgeInsets.all(0),
+                  enableDivider: false,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 52.1,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: BtvColors.seperatorOnLight,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(100))),
+                    side: const BorderSide(
+                        width: 1, color: BtvColors.seperatorOnLight),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(
+                        context, BottomSheetSelectResult(cancelled: true));
+                  },
+                  child: Text(
+                    'Cancel',
+                    style:
+                        BtvTextStyles.button1.copyWith(color: BtvColors.label1),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
