23f9d9548ef2ea8f297f5b01ea06ad787d691748
Eliezer Benjamin <eliezer@bccyep.no>
2022-11-16 15:14:25 +0100

    Message section

---
+++ lib/components/message_section.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+import '../graphql/queries/page.graphql.dart';
+import '../services/utils.dart';
+
+class MessageSection extends StatelessWidget {
+  final Fragment$Section$$MessageSection data;
+
+  const MessageSection(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (data.messages == null) {
+      return const SizedBox.shrink();
+    }
+    return Column(
+      children: data.messages!.map((messageData) {
+        return Container(
+          margin: messageData != data.messages!.last ? const EdgeInsets.only(bottom: 16) : null,
+          child: _MessageItem(messageData),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class _MessageItem extends StatelessWidget {
+  final Fragment$Section$$MessageSection$messages data;
+  const _MessageItem(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    final borderColor = getColorFromHex(data.style.border);
+    final textColor = getColorFromHex(data.style.text);
+    if (data.content == '') {
+      return const SizedBox.shrink();
+    }
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: getColorFromHex(data.style.background),
+        border: borderColor != null ? Border.all(color: borderColor) : null,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: MarkdownBody(
+        data: data.content,
+        styleSheet: MarkdownStyleSheet.fromTheme(
+          Theme.of(context).copyWith(
+            textTheme: Theme.of(context).textTheme.apply(bodyColor: textColor, displayColor: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import 'package:intl/intl.dart';
 
 String getFormattedDuration(int durSeconds) {
@@ -49,4 +51,22 @@ bool isComingSoon(String? prodDate) {
     return false;
   }
   return DateTime.now().isBefore(parsedDateTime);
+}
+
+// Parse color from hex string in the formats "aabbcc", "#aabbcc", "ffaabbcc", "#ffaabbcc"
+Color? getColorFromHex(String hexString) {
+  final buffer = StringBuffer();
+  hexString = hexString.replaceFirst('#', '');
+  if (hexString.length != 6 && hexString.length != 8) {
+    return null;
+  }
+  if (hexString.length == 6) {
+    buffer.write('ff');
+  }
+  buffer.write(hexString);
+  final color = int.tryParse(buffer.toString(), radix: 16);
+  if (color == null) {
+    return null;
+  }
+  return Color(color);
 }
