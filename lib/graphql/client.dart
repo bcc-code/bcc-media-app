c4f4e7341352f1b1bc997fa20ab7f7c9f1fa4027
Andreas Gangsø <andreas.gangso@bcc.no>
2022-11-22 16:34:59 +0100

    chore: update packages

--- lib/graphql/client.dart
+++ lib/graphql/client.dart
@@ -1,17 +1,11 @@
-import 'dart:convert';
-
 import 'package:brunstadtv_app/providers/auth_state.dart';
 import 'package:brunstadtv_app/providers/settings_service.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gql_dio_link/gql_dio_link.dart';
 import 'package:graphql/client.dart';
 import 'package:http/http.dart';
-import 'package:http/io_client.dart';
 import 'package:http/retry.dart';
 
 import '../env/env.dart';
-import '../main.dart';
 
 final gqlClientProvider = Provider<GraphQLClient>((ref) {
   final httpLink = HttpLink(Env.brunstadtvApiEndpoint,
