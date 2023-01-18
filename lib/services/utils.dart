a0529927e6f211f83a55c39937bc0b982641df91
Eliezer Benjamin <eliezer@bccyep.no>
2023-01-18 13:48:47 +0100

    Do not cache UTC offset

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -5,10 +5,6 @@ import 'package:intl/intl.dart';
 import '../components/feature_badge.dart';
 import '../helpers/btv_colors.dart';
 
-class Globals {
-  static final utcOffset = _getUtcOffset();
-}
-
 String getFormattedDuration(int durSeconds, {bool padFirstSegment = false}) {
   final duration = Duration(seconds: durSeconds);
 
@@ -116,7 +112,7 @@ Rect iPadSharePositionOrigin(BuildContext context) {
 }
 
 /// Get current UTC offset in the format 'hh:mm'
-String _getUtcOffset() {
+String getUtcOffset() {
   final timeZoneOffset = DateTime.now().timeZoneOffset;
   final hours = timeZoneOffset.inHours.toString().padLeft(2, '0');
   final minutes = (timeZoneOffset.inMinutes % 60).toString().padLeft(2, '0');
@@ -126,7 +122,7 @@ String _getUtcOffset() {
 /// Get date in ISO8601 format with UTC offset
 String getFormattedDateTime(DateTime date) {
   final localDateTime = DateFormat('yyyy-MM-ddTHH:mm:ss').format(date);
-  return '$localDateTime+${Globals.utcOffset}';
+  return '$localDateTime+${getUtcOffset()}';
 }
 
 /// Check if startTime < Now < endTime
