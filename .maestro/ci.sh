50846e601b8daf2f8dc4c1db94469cb782a9f1dd
Andreas Gangsø <andreas.gangso@bcc.no>
2023-02-17 10:18:45 +0100

    fix ci script

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -7,6 +7,6 @@ curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
 #run maestro e2e tests
-envsubst < .env.js.template > .env.js
+envsubst < flows/.env.js.template > flows/.env.js
 make ios-build
 maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/
