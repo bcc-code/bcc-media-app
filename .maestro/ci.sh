78c21ce8d9013790a049881e135322cee3318f56
Andreas Gangsø <andreas.gangso@bcc.no>
2023-02-17 14:59:15 +0100

    ci: fix ordering of commands

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -9,11 +9,18 @@ export PATH="$PATH":"$HOME/.maestro/bin"
 #run maestro e2e tests
 envsubst < flows/scripts/.env.js.template > flows/scripts/.env.js
 flutter doctor -v
-make ios-build
+
+# backup and replace podspec
 mv ../external_projects/bccm_player/ios/bccm_player.podspec bccm_player.podspec
 mv ../external_projects/bccm_player/ios/bccm_player_simulator.podspec.template ../external_projects/bccm_player/ios/bccm_player.podspec
-maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/ || exit 1
+
+# build the app for the simulator
+make ios-build
+
+# restore podspec
 rm ../external_projects/bccm_player/ios/bccm_player.podspec
 mv bccm_player.podspec ../external_projects/bccm_player/ios/bccm_player.podspec
 cat ../external_projects/bccm_player/ios/bccm_player.podspec
+
+maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/ || exit 1
 exit 0
