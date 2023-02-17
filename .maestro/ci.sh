d91f3398298279ca54c84a33bf6acde2080a68d7
Andreas Gangsø <andreas.gangso@bcc.no>
2023-02-17 14:53:57 +0100

    ci: use local googlecast.xcframework only in maestro tests

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -10,5 +10,10 @@ export PATH="$PATH":"$HOME/.maestro/bin"
 envsubst < flows/scripts/.env.js.template > flows/scripts/.env.js
 flutter doctor -v
 make ios-build
+mv ../external_projects/bccm_player/ios/bccm_player.podspec bccm_player.podspec
+mv ../external_projects/bccm_player/ios/bccm_player_simulator.podspec.template ../external_projects/bccm_player/ios/bccm_player.podspec
 maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/ || exit 1
+rm ../external_projects/bccm_player/ios/bccm_player.podspec
+mv bccm_player.podspec ../external_projects/bccm_player/ios/bccm_player.podspec
+cat ../external_projects/bccm_player/ios/bccm_player.podspec
 exit 0
