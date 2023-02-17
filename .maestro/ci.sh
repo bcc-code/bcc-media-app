5135ef06812c93e7172cec5d15629b5bf2b426cd
Andreas Gangsø <andreas.gangso@bcc.no>
2023-02-17 11:15:45 +0100

    flutter doctor before test build

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -8,6 +8,7 @@ export PATH="$PATH":"$HOME/.maestro/bin"
 
 #run maestro e2e tests
 envsubst < flows/scripts/.env.js.template > flows/scripts/.env.js
+flutter doctor -v
 make ios-build
 maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/ || exit 1
 exit 0
