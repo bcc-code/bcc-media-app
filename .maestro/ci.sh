cb1266066cf748a57d30eeb7de894dfa044edd02
Andreas Gangsø <andreas.gangso@bcc.no>
2023-03-08 12:15:22 +0100

    fix: remove the podspec hack

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -10,17 +10,8 @@ export PATH="$PATH":"$HOME/.maestro/bin"
 envsubst < flows/scripts/.env.js.template > flows/scripts/.env.js
 flutter doctor -v
 
-# backup and replace podspec
-mv ../external_projects/bccm_player/ios/bccm_player.podspec bccm_player.podspec
-mv ../external_projects/bccm_player/ios/bccm_player_simulator.podspec.template ../external_projects/bccm_player/ios/bccm_player.podspec
-
 # build the app for the simulator
 make ios-build
-
-# restore podspec
-rm ../external_projects/bccm_player/ios/bccm_player.podspec
-mv bccm_player.podspec ../external_projects/bccm_player/ios/bccm_player.podspec
-cat ../external_projects/bccm_player/ios/bccm_player.podspec
 
 maestro cloud -e PLATFORM=ios --apiKey $MAESTRO_API_KEY ../build/ios/iphonesimulator/Runner.app flows/ || exit 1
 exit 0
