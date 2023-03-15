8012a045d7ae307445f1710d318c35eda3bd62f0
Eliezer Benjamin <eliezer@bccyep.no>
2023-03-15 09:06:34 +0100

    Store answers

--- lib/components/surveys/bottom_sheet/survey_questions.dart
+++ lib/components/surveys/bottom_sheet/survey_questions.dart
@@ -6,22 +6,55 @@ import 'package:flutter/material.dart';
 
 import '../../text_field_input.dart';
 
-class SurveyQuestions extends StatelessWidget {
+abstract class SurveyAnswer {}
+
+class SurveyRatingAnswer implements SurveyAnswer {
+  final String id;
+  final int rating;
+  SurveyRatingAnswer({required this.id, required this.rating});
+}
+
+class SurveyQuestionAnswer implements SurveyAnswer {
+  final String id;
+  final String answer;
+  SurveyQuestionAnswer({required this.id, required this.answer});
+}
+
+class SurveyQuestions extends StatefulWidget {
   final List<Fragment$SurveyQuestion> surveyQuestions;
 
   const SurveyQuestions(this.surveyQuestions, {super.key});
+
+  @override
+  State<SurveyQuestions> createState() => _SurveyQuestionsState();
+}
+
+class _SurveyQuestionsState extends State<SurveyQuestions> {
+  final Map<String, SurveyAnswer> surveyAnswers = {};
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
-      children: surveyQuestions.map(
+      children: widget.surveyQuestions.map(
         (question) {
           if (question is Fragment$SurveyQuestion$$SurveyRatingQuestion) {
-            return SurveyQuestionRating(question);
+            return SurveyQuestionRating(
+              question: question,
+              onRatingChanged: (rating) {
+                surveyAnswers[question.id] = SurveyRatingAnswer(id: question.id, rating: rating);
+                print('ratingAnswers: $surveyAnswers');
+              },
+            );
           } else if (question is Fragment$SurveyQuestion$$SurveyTextQuestion) {
-            return SurveyQuestionText(question);
+            return SurveyQuestionText(
+              question: question,
+              onAnswerChanged: (answer) {
+                surveyAnswers[question.id] = SurveyQuestionAnswer(id: question.id, answer: answer);
+                print('questionAnswers: $surveyAnswers');
+              },
+            );
           }
           return const SizedBox.shrink();
         },
@@ -32,8 +65,13 @@ class SurveyQuestions extends StatelessWidget {
 
 class SurveyQuestionRating extends StatefulWidget {
   final Fragment$SurveyQuestion$$SurveyRatingQuestion question;
+  final Function(int) onRatingChanged;
 
-  const SurveyQuestionRating(this.question, {super.key});
+  const SurveyQuestionRating({
+    super.key,
+    required this.question,
+    required this.onRatingChanged,
+  });
 
   @override
   State<SurveyQuestionRating> createState() => _SurveyQuestionRatingState();
@@ -65,6 +103,7 @@ class _SurveyQuestionRatingState extends State<SurveyQuestionRating> {
             rating: curRating,
             onRatingChanged: (rating) {
               setState(() => curRating = rating);
+              widget.onRatingChanged(rating);
             },
           ),
         ],
@@ -73,11 +112,33 @@ class _SurveyQuestionRatingState extends State<SurveyQuestionRating> {
   }
 }
 
-class SurveyQuestionText extends StatelessWidget {
+class SurveyQuestionText extends StatefulWidget {
   final Fragment$SurveyQuestion$$SurveyTextQuestion question;
+  final Function(String) onAnswerChanged;
+
+  const SurveyQuestionText({
+    super.key,
+    required this.question,
+    required this.onAnswerChanged,
+  });
+
+  @override
+  State<SurveyQuestionText> createState() => _SurveyQuestionTextState();
+}
+
+class _SurveyQuestionTextState extends State<SurveyQuestionText> {
   final textController = TextEditingController();
 
-  SurveyQuestionText(this.question, {super.key});
+  @override
+  void initState() {
+    super.initState();
+    textController.addListener(() {
+      final answer = textController.text;
+      if (answer.trim().isNotEmpty) {
+        widget.onAnswerChanged(answer);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,13 +149,13 @@ class SurveyQuestionText extends StatelessWidget {
         children: [
           Container(
             margin: const EdgeInsets.only(bottom: 4),
-            child: Text(question.title, style: BccmTextStyles.title2),
+            child: Text(widget.question.title, style: BccmTextStyles.title2),
           ),
-          if (question.description != null)
+          if (widget.question.description != null)
             Container(
               margin: const EdgeInsets.only(bottom: 8),
               child: Text(
-                question.description!,
+                widget.question.description!,
                 style: BccmTextStyles.body2.copyWith(color: BccmColors.label3),
               ),
             ),
