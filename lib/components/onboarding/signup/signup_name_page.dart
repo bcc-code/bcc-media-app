5ad6bb648409b691dd3d39df07a8976913c0a724
Andreas Gangsø <andreas.gangso@bcc.no>
2023-03-31 10:11:51 +0200

    tweak(signup): only one formfillgroup

--- lib/components/onboarding/signup/signup_name_page.dart
+++ lib/components/onboarding/signup/signup_name_page.dart
@@ -52,85 +52,83 @@ class SignupNamePage extends HookWidget implements SignupScreenPage {
 
     return Form(
       key: formKey.value,
-      child: AutofillGroup(
-        child: OnboardingPageWrapper(
-          title: 'Your name',
-          description: "What's your name?",
-          body: [
-            Padding(
-              padding: const EdgeInsets.only(top: 48, bottom: 10),
-              child: Text(
-                'First name',
-                style: BccmTextStyles.caption1.copyWith(color: BccmColors.label2),
-              ),
+      child: OnboardingPageWrapper(
+        title: 'Your name',
+        description: "What's your name?",
+        body: [
+          Padding(
+            padding: const EdgeInsets.only(top: 48, bottom: 10),
+            child: Text(
+              'First name',
+              style: BccmTextStyles.caption1.copyWith(color: BccmColors.label2),
             ),
-            TextFormField(
-              autofocus: true,
-              autofillHints: const [AutofillHints.givenName],
-              controller: firstNameController,
-              focusNode: firstNameFocusNode,
-              keyboardType: TextInputType.emailAddress,
-              autovalidateMode: AutovalidateMode.disabled,
-              style: BccmTextStyles.body2.copyWith(color: BccmColors.label1),
-              cursorColor: BccmColors.tint1,
-              cursorWidth: 1,
-              validator: (val) => val?.isNotEmpty == true ? null : 'Required',
-              decoration: BccmInputDecorations.textFormField.copyWith(hintText: 'Type in first name'),
-              onEditingComplete: () => lastNameFocusNode.requestFocus(),
+          ),
+          TextFormField(
+            autofocus: true,
+            autofillHints: const [AutofillHints.givenName],
+            controller: firstNameController,
+            focusNode: firstNameFocusNode,
+            keyboardType: TextInputType.emailAddress,
+            autovalidateMode: AutovalidateMode.disabled,
+            style: BccmTextStyles.body2.copyWith(color: BccmColors.label1),
+            cursorColor: BccmColors.tint1,
+            cursorWidth: 1,
+            validator: (val) => val?.isNotEmpty == true ? null : 'Required',
+            decoration: BccmInputDecorations.textFormField.copyWith(hintText: 'Type in first name'),
+            onEditingComplete: () => lastNameFocusNode.requestFocus(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 10),
+            child: Text(
+              'Last name',
+              style: BccmTextStyles.caption1.copyWith(color: BccmColors.label2),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 12, bottom: 10),
-              child: Text(
-                'Last name',
-                style: BccmTextStyles.caption1.copyWith(color: BccmColors.label2),
-              ),
-            ),
-            TextFormField(
-              autofillHints: const [AutofillHints.familyName],
-              controller: lastNameController,
-              focusNode: lastNameFocusNode,
-              keyboardType: TextInputType.emailAddress,
-              autovalidateMode: AutovalidateMode.disabled,
-              style: BccmTextStyles.body2.copyWith(color: BccmColors.label1),
-              cursorColor: BccmColors.tint1,
-              cursorWidth: 1,
-              validator: (val) => val?.isNotEmpty == true ? null : 'Required',
-              decoration: BccmInputDecorations.textFormField.copyWith(hintText: 'Type in last name'),
-              onEditingComplete: nextPage,
-            ),
-          ],
-          bottomArea: [
-            Container(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: BtvButton.largeSecondary(
-                          onPressed: () {
-                            pageController.previousPage(duration: const Duration(milliseconds: 500), curve: Curves.easeOutExpo);
-                            FocusManager.instance.primaryFocus?.unfocus();
-                          },
-                          labelText: S.of(context).back,
-                        ),
+          ),
+          TextFormField(
+            autofillHints: const [AutofillHints.familyName],
+            controller: lastNameController,
+            focusNode: lastNameFocusNode,
+            keyboardType: TextInputType.emailAddress,
+            autovalidateMode: AutovalidateMode.disabled,
+            style: BccmTextStyles.body2.copyWith(color: BccmColors.label1),
+            cursorColor: BccmColors.tint1,
+            cursorWidth: 1,
+            validator: (val) => val?.isNotEmpty == true ? null : 'Required',
+            decoration: BccmInputDecorations.textFormField.copyWith(hintText: 'Type in last name'),
+            onEditingComplete: nextPage,
+          ),
+        ],
+        bottomArea: [
+          Container(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: BtvButton.largeSecondary(
+                        onPressed: () {
+                          pageController.previousPage(duration: const Duration(milliseconds: 500), curve: Curves.easeOutExpo);
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        labelText: S.of(context).back,
                       ),
-                      const SizedBox(width: 12),
-                      Expanded(
-                        child: BtvButton.large(
-                          disabled: firstNameController.value.text.isBlank || lastNameController.value.text.isBlank,
-                          onPressed: nextPage,
-                          labelText: S.of(context).continueButton,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: BtvButton.large(
+                        disabled: firstNameController.value.text.isBlank || lastNameController.value.text.isBlank,
+                        onPressed: nextPage,
+                        labelText: S.of(context).continueButton,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
