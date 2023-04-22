cda5ea28fd9e523b4e703f5b10875f0fca5ac507
Coen Wessels <accounts@cwessels.nl>
2023-04-22 17:33:52 +0200

    fix(android): Keep playing when user locks phone

--- external_projects/bccm_player/android/src/main/kotlin/media/bcc/bccm_player/BccmPlayerPluginEvent.kt
+++ external_projects/bccm_player/android/src/main/kotlin/media/bcc/bccm_player/BccmPlayerPluginEvent.kt
@@ -1,6 +1,7 @@
 package media.bcc.bccm_player
 
 import android.app.Activity
+import androidx.lifecycle.Lifecycle
 
 interface BccmPlayerPluginEvent
 
@@ -10,7 +11,7 @@ class OnActivityStop : BccmPlayerPluginEvent
 class SetPlayerViewVisibilityEvent(val viewId: Long, val visible: Boolean) :
     BccmPlayerPluginEvent
 
-class PictureInPictureModeChangedEvent2(val isInPictureInPictureMode: Boolean) :
+class PictureInPictureModeChangedEvent2(val isInPictureInPictureMode: Boolean, val lifecycleState: Lifecycle.State) :
     BccmPlayerPluginEvent
 
 class PictureInPictureModeChangedEvent(
