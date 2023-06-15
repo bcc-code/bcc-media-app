45a515a539962ea1b4e9d63b6a3f23ee9a2747f3
Andreas Gangsø <andreas.gangso@bcc.no>
2023-06-15 16:10:58 +0200

    wip: PopupMenuWidget

--- external_projects/bccm_player/lib/src/widgets/controls/default_controls.dart
+++ external_projects/bccm_player/lib/src/widgets/controls/default_controls.dart
@@ -1,7 +1,10 @@
 import 'package:bccm_player/bccm_player.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'package:flutter/material.dart';
+
+import 'popup_menu.dart';
 
 class DefaultControls extends HookWidget {
   const DefaultControls({
@@ -62,6 +65,7 @@ class DefaultControls extends HookWidget {
                 },
               ),
             ),
+            _SettingsWidget(),
             IconButton(
               icon: Icon(
                 isFullscreen ? Icons.fullscreen_exit : Icons.fullscreen,
@@ -82,5 +86,102 @@ class DefaultControls extends HookWidget {
     );
 
     return isFullscreen ? SafeArea(child: w) : w;
+  }
+}
+
+/// An arbitrary widget that lives in a popup menu
+class PopupMenuWidget extends PopupMenuEntry<Never> {
+  const PopupMenuWidget({super.key, required this.height, required this.child});
+
+  final Widget child;
+
+  @override
+  final double height;
+
+  @override
+  _PopupMenuWidgetState createState() => _PopupMenuWidgetState();
+
+  @override
+  bool represents(value) => false;
+}
+
+class _PopupMenuWidgetState extends State<PopupMenuWidget> {
+  bool clicked = false;
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
+
+class _SettingsWidget extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      offset: Offset(0, 50),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      padding: EdgeInsets.zero,
+      elevation: 0,
+      constraints: null,
+      surfaceTintColor: Colors.transparent,
+      itemBuilder: (context) => <PopupMenuEntry>[
+        PopupMenuWidget(
+          height: 100.0,
+          child: ClipRRect(borderRadius: BorderRadius.circular(16), child: SettingsTabs()),
+        ),
+      ],
+    );
+  }
+}
+
+class SettingsTabs extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final color = PopupMenuTheme.of(context).color ?? Theme.of(context).colorScheme.surface;
+    final height = useState(100.0);
+    return Container(
+      color: color,
+      width: 150,
+      height: height.value,
+      child: Navigator(
+        onGenerateRoute: (routeSettings) {
+          // if audio route
+          switch (routeSettings.name) {
+            case 'audio':
+              height.value = 200;
+              return CupertinoPageRoute(
+                builder: (context) => Container(
+                  color: color,
+                  height: height.value,
+                  width: 150,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      ListTile(dense: true, title: Text('English', style: Theme.of(context).textTheme.labelMedium)),
+                      ListTile(dense: true, title: Text('Norsk', style: Theme.of(context).textTheme.labelMedium)),
+                    ],
+                  ),
+                ),
+              );
+          }
+          return CupertinoPageRoute(
+            builder: (context) => GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('audio');
+              },
+              child: Container(
+                color: color,
+                height: height.value,
+                width: 150,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(dense: true, title: Text('Audio language', style: Theme.of(context).textTheme.labelMedium)),
+                    ListTile(dense: true, title: Text('Subtitle language', style: Theme.of(context).textTheme.labelMedium)),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
