27acded9a5a63401f5a0bc83b113595f74daa0a7
Eliezer Benjamin <eliezer@bccyep.no>
2023-06-19 16:14:05 +0200

    Markdown support for FAQ (#209)

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+import '../theme/design_system/design_system.dart';
 
 String getFormattedAgeRating(String ageRating) {
   return ageRating == 'A' ? '0+' : '$ageRating+';
@@ -54,4 +57,29 @@ void tryCatchRecordError(dynamic Function() fn) {
 Rect iPadSharePositionOrigin(BuildContext context) {
   final Size size = MediaQuery.of(context).size;
   return Rect.fromLTWH(0, 0, size.width, size.height / 2);
+}
+
+MarkdownStyleSheet getMarkdownStyleSheet(BuildContext context) {
+  final design = DesignSystem.of(context);
+  return MarkdownStyleSheet.fromTheme(
+    Theme.of(context).copyWith(
+      textTheme: Theme.of(context).textTheme.apply(bodyColor: design.colors.label3, displayColor: design.colors.label3),
+    ),
+  ).copyWith(
+    horizontalRuleDecoration: BoxDecoration(border: Border.all(width: 1, color: design.colors.separatorOnLight)),
+    h1: design.textStyles.headline1.copyWith(color: design.colors.label3),
+    h2: design.textStyles.headline2.copyWith(color: design.colors.label3),
+    h3: design.textStyles.title1.copyWith(color: design.colors.label3),
+    h4: design.textStyles.title2.copyWith(color: design.colors.label3),
+    h5: design.textStyles.title3.copyWith(color: design.colors.label3),
+    h6: design.textStyles.body1.copyWith(color: design.colors.label3),
+    a: design.textStyles.body2.copyWith(color: DesignSystem.of(context).colors.tint1, decoration: TextDecoration.underline),
+    blockquoteDecoration: BoxDecoration(
+      color: Colors.transparent,
+      border: Border(
+        left: BorderSide(width: 4, color: design.colors.separatorOnLight),
+      ),
+    ),
+    blockquotePadding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+  );
 }
