9b4dc0497ac0c38416ec5ca26d8894034b9b9adb
Haoran Wang <2262791309@qq.com>
2023-10-17 12:08:46 +0200

    clean up searchbar

--- kids/lib/screens/search.dart
+++ kids/lib/screens/search.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
 import 'package:auto_route/auto_route.dart';
-// import 'package:brunstadtv_app/components/status/loading_generic.dart';
-// import 'package:brunstadtv_app/graphql/queries/kids/show.graphql.dart';
-// import 'package:brunstadtv_app/helpers/extensions.dart';
-// import 'package:flutter_hooks/flutter_hooks.dart';
-// import 'package:graphql/client.dart';
-import 'package:kids/components/buttons/button.dart';
-// import 'package:kids/components/grid/episode_grid.dart';
-// import 'package:kids/design_system.dart';
 import 'package:kids/helpers/svg_icons.dart';
 import 'package:brunstadtv_app/theme/design_system/design_system.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_framework/responsive_breakpoints.dart';
-// import 'package:skeletonizer/skeletonizer.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -87,7 +77,7 @@ class SearchBar extends StatelessWidget {
     // final small = bp.smallerThan(TABLET);
     return Container(
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 239, 243, 249), // 很淡的灰色背景
+        color: design.colors.background2, // 很淡的灰色背景
         borderRadius: BorderRadius.circular(30.0), // 圆角边框
       ),
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -99,7 +89,7 @@ class SearchBar extends StatelessWidget {
           ),
           Expanded(
             child: TextField(
-              style: design.textStyles.title1.copyWith(color: Colors.black),
+              style: design.textStyles.title1.copyWith(color: design.colors.label1),
               decoration: const InputDecoration(
                 border: InputBorder.none, // 隐藏输入框边框
                 // hintText: 'Search...', // 提示文本
