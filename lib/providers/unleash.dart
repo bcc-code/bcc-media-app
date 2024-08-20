483a18a1136d7f69f07bcf3ac8e85289d1245df0
Andreas Gangsø <andreasgangso@gmail.com>
2024-08-20 12:30:03 +0200

    feat: add more context to unleash - app version and build number, os and os version

--- lib/providers/unleash.dart
+++ lib/providers/unleash.dart
@@ -8,5 +8,7 @@ final unleashContextProviderOverride = unleashContextProvider.overrideWith((ref)
     gender: ref.watch(authStateProvider.select((value) => value.user?.gender)),
     birthDate: ref.watch(authStateProvider.select((value) => value.user?.birthdate)),
     isBetaTester: ref.watch(settingsProvider.select((s) => s.isBetaTester)),
+    appVersion: formatAppVersion(ref.watch(packageInfoProvider)),
+    appBuildNumber: ref.watch(packageInfoProvider.select((pi) => pi.buildNumber)),
   );
 });
