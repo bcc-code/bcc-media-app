8b468515eb9104cf64308b2f694b81072998a457
Sigve Hansen <sighan12@gmail.com>
2024-11-19 14:25:16 +0100

    chore: update meastro

--- .maestro/ci.sh
+++ .maestro/ci.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 #install maestro
-export MAESTRO_VERSION=1.39.0; curl -Ls "https://get.maestro.mobile.dev" | bash
+export MAESTRO_VERSION=1.39.1; curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
 #run maestro e2e tests
